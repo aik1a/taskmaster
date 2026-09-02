648e2a28594745661864762c6989a52de86638dd
Aikia R <aikia.riv.c@gmail.com>
2026-09-01 23:53:19 -0400

    feat: crear acceso y registro local

--- app/src/main/java/com/example/taskmaster/LoginActivity.java
+++ app/src/main/java/com/example/taskmaster/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.taskmaster;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,5 +9,9 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        findViewById(R.id.btnIrRegistro).setOnClickListener(view ->
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class)));
     }
 }
